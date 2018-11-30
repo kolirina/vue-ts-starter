e175ab0d7bbf456fe8fb3402f4e1962c977e698c
Alexey Filimonov <fay0@yandex.ru>
2018-11-30 17:26:06 +0300

    * numberField

---
+++ src/app/components/iiNumberField.ts
@@ -0,0 +1,14 @@
+// import {UI} from "../app/ui";
+// import Component from "vue-class-component";
+// import {Prop} from "vue-property-decorator";
+import {VTextField} from "vuetify/es5/components";
+// @Component({
+//     // language=Vue
+//     template: `
+//         v-text-field
+//     `
+// })
+export default {
+    name: 'ii-number-field',
+    extends: VTextField
+};
