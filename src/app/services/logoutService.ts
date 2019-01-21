c92dfb68884e508eaaa026520345fa067437feff
nedelko <nedelko@bifit.com>
2019-01-21 09:23:09 +0300

    * правки для nginx

--- src/app/services/logoutService.ts
+++ src/app/services/logoutService.ts
@@ -1,4 +1,4 @@
-import {Container, Inject, Singleton} from "typescript-ioc";
+import {Inject, Singleton} from "typescript-ioc";
 import {Service} from "../platform/decorators/service";
 import {Http} from "../platform/services/http";
 import {Storage} from "../platform/services/storage";
@@ -14,10 +14,9 @@ export class LogoutService {
     private localStorage: Storage;
 
     async logout(): Promise<void> {
-        console.log("BEFORE LOGOUT");
         await this.http.post("/user/logout");
         this.localStorage.delete(StoreKeys.STORE_KEY);
         this.localStorage.delete(StoreKeys.TOKEN_KEY);
-        window.location.assign( "http://localhost:8080/auth.html");
+        window.location.replace("/");
     }
 }
