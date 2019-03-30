24f385d2e2832d2ea14ef2e401caeeb04d319c5f
b1ack <developer@intelinvest.ru>
2019-03-30 15:34:03 +0300

    * исправлен GTM

--- src/assets/js/gtm.js
+++ src/assets/js/gtm.js
@@ -18,4 +18,4 @@
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NCL7WHD');
+})(window,document,'script','dataLayer','GTM-5WP2PK2');
