4ecabbd4ce07e0c21cd20f7394cedf034535d7ee
Yuri Zinchenko <Dlyaoplatmt@gmail.com>
2019-05-21 14:18:42 +0300

    стилизация диалога обновления тарифа

--- src/app/app/appFrame.ts
+++ src/app/app/appFrame.ts
@@ -19,6 +19,7 @@ import {UiStateHelper} from "../utils/uiStateHelper";
 import {MutationType} from "../vuex/mutationType";
 import {StoreType} from "../vuex/storeType";
 import {UI} from "./ui";
+import {TariffExpiredDialog} from "../components/dialog/tariffExpiredDialog";
 
 const MainStore = namespace(StoreType.MAIN);
 
@@ -135,15 +136,13 @@ const MainStore = namespace(StoreType.MAIN);
                     </v-layout>
                 </v-navigation-drawer>
                 <v-content>
-                    <div class="wrapper-for-scroll-content">
-                        <v-container fluid class="paddT0">
-                            <v-slide-y-transition mode="out-in">
-                                <!--<keep-alive :include="cachedPages">-->
-                                <router-view></router-view>
-                                <!--</keep-alive>-->
-                            </v-slide-y-transition>
-                        </v-container>
-                    </div>
+                    <v-container fluid class="paddT0">
+                        <v-slide-y-transition mode="out-in">
+                            <!--<keep-alive :include="cachedPages">-->
+                            <router-view></router-view>
+                            <!--</keep-alive>-->
+                        </v-slide-y-transition>
+                    </v-container>
                     <v-footer color="#f7f9fb" class="footer-app">
                         <v-layout class="footer-app-wrap-content" wrap align-center justify-space-between>
                             <div class="footer-app-wrap-content__text"><i class="far fa-copyright"></i> {{ copyrightInfo }}</div>
--- src/app/components/dialogs/tariffExpiredDialog.ts
+++ src/app/components/dialogs/tariffExpiredDialog.ts
@@ -29,17 +29,18 @@ import {BtnReturn, CustomDialog} from "../../platform/dialogs/customDialog";
                 <v-icon v-if="allowClose" class="closeDialog" @click.native="closeDialog">close</v-icon>
                 <span v-else class="closeDialog">{{ count }}</span>
 
-                <v-card-title class="headline">Закончилась подписка на тарифный план</v-card-title>
-                <v-card-text>
-                    <div>Срок действия вашего тарифного плана истек, рекомендуем обновить подписку.</div>
+                <v-card-title class="bold fs16 margB64">Закончилась подписка на тарифный план</v-card-title>
+                <v-card-text class="paddB128">
+                    <v-layout align-center column>
+                        <v-img src="./img/tariffs/update_tariff.svg" width="100%" heigth="100%" max-width="346" max-height="131"></v-img>
+                        <div class="fs14 mw320 alignC mt-2">Срок действия вашего тарифного плана истек, рекомендуем обновить тарифный план</div>
+                        <div class="margT24">
+                            <v-btn @click="tariffs" color="primary">
+                                Обновить подписку
+                            </v-btn>
+                        </div>
+                    </v-layout>
                 </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" light @click="tariffs">
-                        Обновить подписку
-                        <v-icon right dark small>fas fa-rocket</v-icon>
-                    </v-btn>
-                </v-card-actions>
             </v-card>
         </v-dialog>
     `
