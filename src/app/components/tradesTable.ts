b9743c6e40a3633e61aa729dca3142655f71c9ef
Yuri Zinchenko <Dlyaoplatmt@gmail.com>
2019-05-27 18:45:01 +0300

    замечания

--- src/app/components/tradesTable.ts
+++ src/app/components/tradesTable.ts
@@ -31,7 +31,7 @@ const MainStore = namespace(StoreType.MAIN);
                       :rows-per-page-items="[25, 50, 100, 200]"
                       expand must-sort>
             <template #items="props">
-                <tr @dblclick="props.expanded = !props.expanded">
+                <tr class="selectable" @dblclick="props.expanded = !props.expanded">
                     <td>
                         <span @click="props.expanded = !props.expanded" class="data-table-cell" :class="{'data-table-cell-open': props.expanded, 'path': true}"></span>
                     </td>
