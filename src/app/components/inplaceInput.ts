a2d7206e1e032d85be68747d4aa59d0086864d1e
Yuri Zinchenko <Dlyaoplatmt@gmail.com>
2019-08-08 18:36:15 +0300

    доделал

--- src/app/components/inplaceInput.ts
+++ src/app/components/inplaceInput.ts
@@ -8,28 +8,27 @@ import {UI} from "../app/ui";
 @Component({
     // language=Vue
     template: `
-        <div class="inplace-input">
-            <v-layout @click="focus" :class="['inplace-custom-input', isEditMode ? 'active-inplace-custom-input' : '']" justify-space-between @blur="closeInput">
-                <input ref="inplaceInput" v-model.trim="editableValue" @focus="isEditMode = true" @keyup.enter="emitCompleteEvent"
-                    @keyup.esc="closeInput" :maxlength="maxLength">
-                <v-btn v-if="!isEditMode" @click="setEditMode" flat icon color="indigo">
-                    <i class="profile-edit"></i>
+        <v-layout @click="focus" :class="['inplace-custom-input', isEditMode ? 'active-inplace-custom-input' : '']" justify-space-between>
+            <input ref="inplaceInput" v-model.trim="editableValue" @keyup.enter="emitCompleteEvent"
+                   @keyup.esc="closeInput" :maxlength="maxLength" :placeholder="placeholder">
+            <v-btn v-show="!isEditMode" @click="setEditMode" ref="editBtn" flat icon color="indigo">
+                <i class="profile-edit"></i>
+            </v-btn>
+            <v-layout v-if="isEditMode" class="initial-flex btn-action-section" align-center>
+                <v-btn @click="emitCompleteEvent()" small flat icon color="indigo">
+                    <v-icon>done</v-icon>
                 </v-btn>
-                <v-layout v-if="isEditMode" class="initial-flex btn-action-section" align-center>
-                    <v-btn @click.native="emitCompleteEvent()" small flat icon color="indigo">
-                        <v-icon>done</v-icon>
-                    </v-btn>
-                    <v-btn @click.native="updateEditableValue()" small flat icon color="indigo">
-                        <v-icon>clear</v-icon>
-                    </v-btn>
-                </v-layout>
+                <v-btn @click="closeInput()" small flat icon color="indigo">
+                    <v-icon>clear</v-icon>
+                </v-btn>
             </v-layout>
-        </div>
+        </v-layout>
     `
 })
 export class InplaceInput extends UI {
 
     $refs: {
+        editBtn: any,
         inplaceInput: HTMLInputElement
     };
 
@@ -56,7 +55,15 @@ export class InplaceInput extends UI {
      */
     mounted(): void {
         this.updateEditableValue();
-        console.log(this.$refs);
+        window.addEventListener("click", (event: any) => {
+            if (event.path.find((element: object) => element === this.$refs.editBtn.$vnode.elm)) {
+                this.$refs.inplaceInput.focus();
+                this.isEditMode = true;
+            } else {
+                this.$refs.inplaceInput.blur();
+                this.isEditMode = false;
+            }
+        });
     }
 
     /**
@@ -87,12 +94,9 @@ export class InplaceInput extends UI {
     }
 
     private closeInput(): void {
-        this.$nextTick(() => {
-            this.updateEditableValue();
-            this.$refs.inplaceInput.blur();
-            this.isEditMode = false;
-            console.log(this.isEditMode);
-        });
+        this.updateEditableValue();
+        this.$refs.inplaceInput.blur();
+        this.isEditMode = false;
     }
 
 }
