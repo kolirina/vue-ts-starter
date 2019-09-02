b000c4b62b64f51789bda5db44183f71d88b9186
nedelko <nedelko@bifit.com>
2019-09-02 09:52:43 +0300

    корректировка

--- src/app/components/iiNumberField.ts
+++ src/app/components/iiNumberField.ts
@@ -21,14 +21,11 @@ export class IINumberField extends Vue {
     private onInput(e: any): void {
         (this as any).internalChange = true;
         (this as any).resetSelections(e.target);
-        (this as any).internalValue = this.delimiterСheck(e.target.value);
+        (this as any).internalValue = this.correctDelimiter(e.target.value);
         (this as any).badInput = e.target.validity && e.target.validity.badInput;
     }
 
-    private delimiterСheck(value: string): string {
-        if (value.includes(",")) {
-            return value.replace(",", ".");
-        }
-        return value;
+    private correctDelimiter(value: string): string {
+        return value && value.includes(",") ? value.replace(",", ".") : value;
     }
 }
