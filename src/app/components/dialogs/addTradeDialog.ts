c84ec54a640da4570e7785d379a9c1e1a12c0f2d
stanislav_mac <b1ack@intelinvest.ru>
2019-12-25 18:17:05 +0600

    - исправлено редактирование сделки с НДК меньше 0,01

--- src/app/components/dialogs/addTradeDialog.ts
+++ src/app/components/dialogs/addTradeDialog.ts
@@ -843,6 +843,11 @@ export class AddTradeDialog extends CustomDialog<TradeDialogData, boolean> imple
         this.facevalue = TradeUtils.decimal(this.data.tradeFields.facevalue);
         this.nkd = TradeUtils.decimal(this.data.tradeFields.nkd);
         this.perOne = true;
+        if (this.isBondTrade && new Decimal(this.nkd).comparedTo(new Decimal("0.01")) < 0) {
+            const nkdValue = new Decimal(this.nkd);
+            this.perOne = false;
+            this.nkd = nkdValue.mul(new Decimal(this.quantity).toDP(2, Decimal.ROUND_HALF_UP)).toString();
+        }
         this.fee = TradeUtils.decimal(this.data.tradeFields.fee);
         this.note = this.data.tradeFields.note;
         this.keepMoney = this.data.tradeFields.keepMoney;
