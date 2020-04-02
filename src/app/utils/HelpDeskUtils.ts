4f80f9cf11d8d43365fba366871112565da4e669
b1ack <developer@intelinvest.ru>
2020-04-02 15:13:56 +0300

    * виджет помощи

--- src/app/utils/HelpDeskUtils.ts
+++ src/app/utils/HelpDeskUtils.ts
@@ -43,7 +43,7 @@ export class HelpDeskUtils {
     }
 
     private static initCallback(user: Client): void {
-        window.__widgetInitCallback = (widget: UseDeskWidget) => {
+        window.__widgetInitCallback = (widget: UseDeskWidget): void => {
             const expectedWidget = window.usedeskMessenger;
             expectedWidget.identify({
                 email: user.email,
