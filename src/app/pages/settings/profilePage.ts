a3bd45102f3845b0f1fef6d732802d1f851aefcd
egorov <egorov@bifit.com>
2020-05-27 00:49:38 +0300

    Верстка главной страницы Профиля

--- src/app/pages/settings/profilePage.ts
+++ src/app/pages/settings/profilePage.ts
@@ -10,10 +10,51 @@ import {Component, UI} from "../../app/ui";
                 </v-card-title>
             </v-card>
             <v-layout class="profile" column>
-                <div @click="goToMainSetting">Основные настройки</div>
-                <div @click="goToPaymentSetting">Способ оплаты</div>
-                <div @click="goToInterfaceSetting">Настройки интерфейса</div>
-                <div @click="goToSubscriptionsSetting">Подписки</div>
+                <div class="profile__items">
+                    <div @click="goToMainSetting" class="profile-item">
+                        <div class="profile-item__header">
+                            <img src="./img/profile/main-settings.svg" alt="">
+                            <span>Основные настройки</span>
+                        </div>
+                        <ul>
+                            <li>Смена пароля</li>
+                            <li>Смена Email</li>
+                        </ul>
+                        <ul>
+                            <li>Публичное имя</li>
+                            <li>Ссылка на личный сайт</li>
+                        </ul>
+                    </div>
+                    <div @click="goToPaymentSetting" class="profile-item">
+                        <div class="profile-item__header">
+                            <img src="./img/profile/payment-method.svg" alt="">
+                            <span>Способ оплаты</span>
+                        </div>
+                        <ul>
+                            <li>Тарифный план</li>
+                            <li>Способ оплаты</li>
+                        </ul>
+                    </div>
+                    <div @click="goToInterfaceSetting" class="profile-item">
+                        <div class="profile-item__header">
+                            <img src="./img/profile/interface-settings.svg" alt="">
+                            <span>Настройки интерфейса</span>
+                        </div>
+                        <ul>
+                            <li>Выбор темы</li>
+                            <li>Виджет помощи</li>
+                        </ul>
+                    </div>
+                    <div @click="goToSubscriptionsSetting" class="profile-item">
+                        <div class="profile-item__header">
+                            <img src="./img/profile/subscriptions.svg" alt="">
+                            <span>Подписки</span>
+                        </div>
+                        <ul>
+                            <li>Информационная рассылка</li>
+                        </ul>
+                    </div>
+                </div>
             </v-layout>
         </v-container>
     `
